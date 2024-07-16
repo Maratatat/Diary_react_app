b0fbbf3fec68a16331ba9ac188113e5d15731b06
Marat <447599489marat@gmail.com>
2024-07-16 12:36:37 +0300

    About page added.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-
+            <h1 style={{textAlign:'center'}}>About page</h1>
+            <h3 style={{textAlign:'center'}}>This is diary app to create, update delete and get reports</h3>
         </div>
     );
 };
