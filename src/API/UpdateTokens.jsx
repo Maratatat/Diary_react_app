527c76c620392e40a84fccb8dc935cd30c37693e
Marat <447599489marat@gmail.com>
2024-07-22 10:29:44 +0300

    Removed strict mode, and therefore double execution of code and queries. Alert of not valid(expired) tokens added.

--- src/API/UpdateTokens.jsx
+++ src/API/UpdateTokens.jsx
@@ -4,9 +4,10 @@ import axios from "axios";
 export const UpdateTokens = async (setIsAuth, navigate, callback, service, ...args) => {
     const response = await TokenService.UpdateTokens()
     if (axios.isAxiosError(response)) {
-        /*do modal window message to user*/
+        alert("Sorry, your tokens are not valid. Please authorize again")
         setIsAuth(false)
-        navigate('/auth')
+        localStorage.removeItem("auth")
+        navigate('/auth') 
     } else {
         localStorage.setItem("accessToken", response.data.data.accessToken)
         localStorage.setItem("refreshToken", response.data.data.refreshToken)
